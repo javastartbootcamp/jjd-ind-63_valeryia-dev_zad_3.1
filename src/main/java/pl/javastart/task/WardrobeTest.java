082f463752751261e2b7b37719aef498f71008f7
Valeryia Paliashchuk <vallery7543@gmail.com>
2023-01-26 21:46:21 +0100

    Spacje

--- src/main/java/pl/javastart/task/WardrobeTest.java
+++ src/main/java/pl/javastart/task/WardrobeTest.java
@@ -3,9 +3,9 @@ package pl.javastart.task;
 public class WardrobeTest {
 
     public static void main(String[] args) {
-        Wardrobe war1 = new Wardrobe("Plywood","Black",4);
+        Wardrobe war1 = new Wardrobe("Plywood", "Black", 4);
 
-        Wardrobe war2 = new Wardrobe("MDF","White",6);
+        Wardrobe war2 = new Wardrobe("MDF", "White", 6);
 
         war1.printInfo();
         war2.printInfo();
